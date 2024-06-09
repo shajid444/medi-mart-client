fdf0dbe93f68cc633765efae2dd457a06de4327c
shajid444 <mdshajidhasan76@gmail.com>
2024-06-09 17:42:43 +0600

    after adding helmet provider

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,15 +1,22 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../Pages/Shared/Footer";
 import Navbar from "../Pages/Shared/Navbar";
+import { Helmet } from 'react-helmet-async';
 
 
 const Main = () => {
     return (
         <div>
+            <Helmet>
+        <title>Main page</title>
+  
+      </Helmet>
             <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
+            
         </div>
+        
     );
 };
 
